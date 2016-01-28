b119cb9b7b951677f417d753326dd00d786b8b51
Nik Petersen <demersus@gmail.com>
2016-01-28 10:54:21 -0800

    added spf13 vim files

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm ~/.vim -rf
 cp -r $DIR/vim        ~/.vim
 ln -sf $DIR/vimrc     ~/.vimrc
+
+# SPF13 stuff
+ln -sf $DIR/vimrc.local.spf13     ~/.vimrc.local
+ln -sf $DIR/vimrc.before.local.spf13     ~/.vimrc.before.local
+ln -sf $DIR/vimrc.bundles.local.spf13     ~/.vimrc.bundles.local
+
 ln -sf $DIR/tmux.conf ~/.tmux.conf
 ln -sf $DIR/railsrc   ~/.railsrc
 ln -sf $DIR/gemrc     ~/.gemrc
